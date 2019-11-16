22c1925b0d964bffd510fd5c9df9e5374ea5a305
Kyeong Joo Jung <iamkj03@naver.com>
2019-11-15 20:27:31 -0500

    first commit

---
+++ App/App.cpp
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include "Enclave_u.h"
+#include "sgx_urts.h"
+#include "sgx_utils/sgx_utils.h"
+
+/* Global EID shared by multiple threads */
+sgx_enclave_id_t global_eid = 0;
+const char *data_filename = "sealed.data";
+
+// OCALL implementations
+void print_message(const char* str) {
+    std::cout << str << std::endl;
+}
+
+int ocall_read(int file, void *buf, unsigned int size) {
+    printf("ocall_read(file: %d, void buf: buf, unsigned int: %d): %d\n",file, size, read(file, buf, size));
+    return read(file, buf, size);
+}
+
+int ocall_write(int file, void *buf, unsigned int size) {
+    printf("ocall_write(file: %d, void buf: buf, unsigned int: %d): %d\n",file, size, write(file, buf, size));
+    return write(file, buf, size);
+}
+
+void ocall_close(int file) {
+    printf("ocall_close(file: %d)", file);
+    close(file);
+}
+
+void ocall__exit(int state) {
+     printf("ocall__exit(state: %d)", state);
+     _exit(state);
+}
+    
+int ocall_waitpid(int pid, int *state, int options){
+     printf("ocall_waitpid(pid: %d, state: %d, options: %d): %d\n", pid, &state, options);
+     return waitpid(pid, state, options);
+}
+
+int ocall_fork(void){
+     printf("fork succeeded\n");
+     fork();
+}
+
+int* ocall_shmat(int shmid, const char *shmaddr, int shmflg){
+     printf("what is inside shmat(shmid: %d, shmaddr: , shmflg: %d): %x\n", shmid, shmflg, shmat(shmid, shmaddr, shmflg));
+     return shmat (shmid, shmaddr, shmflg); //print something here
+}
+
+char ocall_getenv (const char *name){
+     printf("ocall_getenv(%s) called\n", name);
+     printf("Path is %s\n", getenv(name));
+     return *getenv(name);
+}
+
+
+int read_password() {
+  int password;
+  std::cout << "Password : ";
+  std::cin >> password;
+  return std::cin.fail() ? -1 : password;
+}
+
+int load_data(uint8_t *sealed_data, size_t sealed_size) {
+  FILE *fp = fopen(data_filename, "rb");
+  if (fp == NULL) {
+      printf("\"%s\"No Sealed.data. Creating new one...\n", data_filename);
+      initialize_enclave_data(global_eid, sealed_data, sealed_size, 0);
+  } else {
+      size_t read_num = fread(sealed_data, 1, sealed_size, fp);
+      if (read_num != sealed_size) {
+          printf("Warning: Failed to read sealed data from \"%s\" (%zu bytes read).\n", data_filename, read_num);
+          return -1;
+      }
+      fclose(fp);
+  }
+}
+
+int save_data(uint8_t *sealed_data, size_t sealed_size) {
+  FILE *fp = fopen("sealed.data", "wb");
+  if (fp) {
+      size_t write_num = fwrite(sealed_data, 1, sealed_size, fp);
+      if (write_num != sealed_size) {
+          printf("Warning: Failed to save sealed data to \"%s\" (%zu bytes written).\n", data_filename, write_num);
+          return -1;
+        }
+      fclose(fp);
+  }
+}
+
+int main(int argc, char const *argv[]) {
+    int password;
+    char id[16];
+
+    if (initialize_enclave(&global_eid, "enclave.token", "enclave.signed.so") < 0) {
+        std::cout << "Fail to initialize enclave." << std::endl;
+        return 1;
+    }
+    size_t sealed_size = sizeof(sgx_sealed_data_t) + sizeof(int);
+    uint8_t* sealed_data = (uint8_t*) malloc(sealed_size);
+
+    load_data(sealed_data, sealed_size);
+    std::cout << "Login ID : ";
+    std::cin >> id;
+
+    if((password = read_password()) < 0) {
+	std::cout << "Provide a correct integer password!" << std::endl;
+	return -1;
+    }
+    
+
+    if (load_data(sealed_data, sealed_size) < 0) {
+      return -1;
+    };
+
+    std::cout << "load_data(" << sealed_size << ", " << &sealed_data << ") : " << load_data(sealed_data, sealed_size) << std::endl;
+    
+    if(strcmp(id,"root") == 0){
+        std::cout << "If id is root, Before EXECUTING ECALL (execute_secure_operation(global_eid, sealed_data, sealed_size, password)" << std::endl;
+	execute_secure_operation(global_eid, sealed_data, sealed_size, password);
+        std::cout << "After EXECUTING ECALL (executing_secure_operation(global_eid, sealed_data, sealded_size, password)" << std::endl;}
+    else{
+	std::cout << "Incorrect login" << std::endl;}
+
+    if (save_data(sealed_data, sealed_size) < 0) {
+      return -1;
+    }
+
+    return 0;
+}
---
+++ Enclave/Enclave.cpp
@@ -0,0 +1,138 @@
+#include <cstdio>
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include "Enclave_t.h"
+#include <stdlib.h>
+
+char message[200];
+
+extern "C" char getenv (const char *name){
+	char ret;
+	if(ocall_getenv(&ret, name) != SGX_SUCCESS) return -1;
+	snprintf(message, 200, "*ret: %d\n", &ret);
+	print_message(message);
+	return ret;
+}
+
+extern "C" int shmat (int shmid, const char *shmaddr, int shmflg){
+
+	int *ret;
+        if (ocall_shmat(&ret, shmid, shmaddr, shmflg) != SGX_SUCCESS) return -1;
+        return *ret;
+}
+
+
+int stdin = 0, stdout = 1, stderr = 2;
+
+extern "C" int read(int file, void *buf, unsigned int size) {
+    int ret;
+    if (ocall_read(&ret, file, buf, size) != SGX_SUCCESS) return -1;
+    return ret;
+}
+
+extern "C" int write(int file, void *buf, unsigned int size) {
+    int ret;
+    if (ocall_write(&ret, file, buf, size) != SGX_SUCCESS) return -1;
+    return ret;
+}
+
+extern "C" void close(int file) {
+    ocall_close(file);
+}
+
+extern "C" int fork(void){
+    int ret;
+    if (ocall_fork(&ret) != SGX_SUCCESS) return -1;
+    return ret;
+    /*return 0;*/
+}
+
+extern "C" int waitpid(int pid, int *state, int options){
+    int ret;
+    if (ocall_waitpid(&ret, pid, state, options) != SGX_SUCCESS) return -1;
+    return ret;
+	/*return 0;*/
+}
+
+extern "C" void _exit (int state){
+    ocall__exit(state);
+}
+
+
+
+void execute_secure_operation(uint8_t* sealed_data, uint32_t sealed_data_size, int password) {
+    int unsealed_value;
+    char tries[400]; //for later use(contains password trials until correct.
+    int shmid;    
+    int shared_memory = 0;    // 공유메모리 공간을 만든다.// 크기는 4byte로 한다. 
+    char data[128];
+    const void *buf = "An error occurred in the read.\n";
+    //const char *p = "PATH";
+    
+    snprintf(tries, 400, "After initialization of unsealed_value, tries, and before initialization before sgx_status status = unseal((sgx_sealded_data_t*) sealed_data, sealed_data_size, (uint8_t*) &unsealed_value, sizeof(unsealed_value)", unsealed_value);
+    print_message(tries);
+
+    sgx_status_t status = unseal((sgx_sealed_data_t*) sealed_data, sealed_data_size,
+            (uint8_t*) &unsealed_value, sizeof(unsealed_value));
+
+    snprintf(tries, 200, "After sgx_status_t status = unseal((sgx_sealed_data_t*) sealed_data, sealed_data_size, (uint8_t*) &unsealed_value, sizeof(unsealed_value));", unsealed_value);
+    print_message(tries);
+
+    if(password == 1234) {
+	snprintf(message, 200, "Successful login", unsealed_value);
+	print_message(message);
+	unsealed_value = password;
+        seal((uint8_t*) &unsealed_value, sizeof(unsealed_value),
+            (sgx_sealed_data_t*) sealed_data, sealed_data_size);}
+    else{
+	snprintf(message, 200, "You have entered wrong password, you have entered ****", password);
+	print_message(message);
+	return;}    
+
+    /* This data should never leave enclave, here it's just for the demo. */
+    snprintf(message, 200, "This is just to check, never should happen. Printing from enclave, just to see what's inside: %d", unsealed_value);
+    print_message(message);
+
+
+
+
+    //TEST
+    snprintf(message, 200, "====================FROM HERE, this is test for function ===============", unsealed_value);
+    print_message(message);
+   
+    shared_memory = shmat(1081345, (char *)0, 0);  
+    snprintf(message, 200, "shmat is %x", shared_memory);
+    print_message(message);  
+
+    /*if(read(0, data, 128)>0){
+       snprintf(message, 200, "read(0, data, 128) is %d", read(0,data,128));
+       print_message(message);
+       write(2, &buf, 31);
+       snprintf(message, 200, "write(2, &buf, 31) is %d", write(2, &buf, 31));
+       print_message(message);
+
+       snprintf(message, 200, "_exit(0); executed", password);
+       print_message(message);       
+       _exit(0);
+
+    }*/
+
+    
+    fork();
+    snprintf(message, 200, "Fork testing worked if printed 2 times", password);
+    print_message(message);
+    
+    snprintf(message, 200, "PATH: %d\n", getenv("PATH"));
+    print_message(message);
+}
+
+void initialize_enclave_data(uint8_t* sealed_data, uint32_t sealed_data_size, int initial_value) {
+    seal((uint8_t*) &initial_value, sizeof(initial_value),
+            (sgx_sealed_data_t*) sealed_data, sealed_data_size);
+
+    /* This data should never leave enclave, here it's just for the demo. */
+    snprintf(message, 200, "Initializing enclave data with value: %d", initial_value);
+    print_message(message);
+}
